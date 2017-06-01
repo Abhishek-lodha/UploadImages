a95254aa40994157b83b6ad727c64853fc4b2d49
Abhishek Lodha <alodha21051992@gmail.com>
2017-06-01 17:43:22 +0530

    adding request details in mongodb

--- server.js
+++ server.js
@@ -5,7 +5,21 @@ const express = require("express"),
   path = require("path"),
   multer = require("multer"),
   cors = require("cors"),
-  winston = require('winston');
+  winston = require('winston'),
+  mongo = require('mongodb'),
+  mongoose = require('mongoose');
+
+let Schema = mongoose.Schema;
+let schemaName = new Schema({
+  originalname: String,
+  name: String,
+  imagePath: String,
+  thumbnailPath: String
+}, {
+  collection: 'collectionName'
+});
+var Model = mongoose.model('Model', schemaName);
+mongoose.connect('mongodb://localhost:27017/uploadImages');
 
 let upload = multer({
   dest: __dirname + '/public/uploads/',
@@ -36,7 +50,17 @@ app.get('/', (req, res) => {
 
 app.post('/upload', upload.single('image'), (req, res) => {
   logger.info(req.file);
-  res.send(req.file.originalname + " saved successfully");
+  var savedata = new Model({
+    'originalname': req.file.originalname,
+    'name': req.file.filename,
+    'imagePath': req.file.path
+  }).save(function(err, result) {
+    if (err) throw err;
+    if (result) {
+      res.json(result);
+    }
+  });
+  // res.send(req.file.originalname + " saved successfully");
 });
 
 app.listen(8080, function() {
