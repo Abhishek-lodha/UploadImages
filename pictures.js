b58d5a148bc7e53d78a2ba457819ba61d21407ed
Abhishek Lodha <alodha21051992@gmail.com>
2017-06-01 14:44:46 +0530

    uploading picture done

---
+++ pictures.js
@@ -0,0 +1,12 @@
+// var express = require('express'),
+//   router = express.Router(),
+//   multer = require('multer')
+//
+// var uploading = multer({
+//   dest: __dirname + '../public/uploads/',
+// });
+//
+// router.post('/pictures/upload', uploading.single('abc'), function(req, res) {
+// });
+//
+// module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 const express = require("express"),
-      app = express(),
-      bodyParser = require('body-parser'),
-      request = require("request"),
-      path = require("path"),
-      multer = require("multer");
+  app = express(),
+  bodyParser = require('body-parser'),
+  request = require("request"),
+  path = require("path"),
+  multer = require("multer"),
+  cors = require("cors");
+
+var upload = multer({
+  dest: __dirname + '/public/uploads/',
+});
+
+app.use(cors());
+
+app.use('/static', express.static(__dirname + '/static'));
+
+app.get('/', (req, res) => {
+  res.sendfile("static/index.html");
+});
+
+app.post('/upload', upload.single('image'), (req, res) => {
+  // res.json({req.files});
+});
+
+app.listen(8080, function() {
+  console.log("Working on port 8080");
+});
+module.exports = app;
